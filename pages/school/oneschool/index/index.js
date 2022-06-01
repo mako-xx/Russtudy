354aa25f76852aca9bb6ef04adff246915a29cbf
CookedMelon <cc2414635846@163.com>
2022-06-01 23:18:13 +0800

    合并

--- pages/school/oneschool/index/index.js
+++ pages/school/oneschool/index/index.js
@@ -1,9 +1,41 @@
-import * as echarts from '../../ec-canvas/echarts';
+import * as echarts from '../../../../ec-canvas/echarts';
 
 const app = getApp()
 const db = wx.cloud.database()
 
 Page({
+  data: {
+    cardCur: 0,
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+    }],
+  },
   dataprocess() {
     var school = this.data.school;
     var schoolpics = this.data.schoolpics;
@@ -144,21 +176,18 @@ Page({
 
   },
   onLoad(options) {
-    var intervalHeight = 10;
-    var SearchHeight = 50;
-    var windowHeight = (app.globalData.ktxWindowHeight) * app.globalData.pxToRpxScale;
-    var topHeight = (app.globalData.ktxStatusHeight + app.globalData.navigationHeight) * app.globalData.pxToRpxScale;
-    var trueHeight = windowHeight;
+    var HeadBar = (app.globalData.ktxStatusHeight + app.globalData.navigationHeight) * app.globalData.pxToRpxScale
+    var ShowHeight = (app.globalData.ktxWindowHeight - app.globalData.ktxStatusHeight - app.globalData.navigationHeight) * app.globalData.pxToRpxScale;
     this.setData({
-      windowHeight: windowHeight,
-      topHeight: topHeight,
-      trueHeight: trueHeight,
-      intervalHeight: intervalHeight,
-      SearchHeight: SearchHeight,
+      HeadBar: HeadBar,
+      ShowHeight: ShowHeight
+    })
+    this.setData({
       school_id: options.school_id,
       loading: true,
       iflove: 0
     })
+    console.log(options.school_id)
     var that = this
     this.setData({
       interval: setInterval(function () {
